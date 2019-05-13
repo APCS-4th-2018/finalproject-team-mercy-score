dddd111794c5e156b610bc953f8a5179455f759d
vinodkm12 <nlvinod.km@gmail.com>
2019-05-13 12:55:16 -0700

    Initalizeds
    -Sincerely,
    
    Vinod

---
+++ TheGame/Double07.java
@@ -0,0 +1,30 @@
+
+/**
+ * General class for player objects of the game, whether they are CPU or player.
+ *
+ * @author Team Mercy Score
+ * @version 5-13-2019
+ */
+public abstract class Double07
+{
+    // instance variables - replace the example below with your own
+    private int p;
+
+    /**
+     * Constructor for objects of class Double07
+     */
+    public Double07()
+    {
+
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void sampleMethod(int y)
+    {   
+    }
+}
---
+++ TheGame/Monitor.java
@@ -0,0 +1,33 @@
+
+/**
+ * Takes in the input. 
+ *
+ * @author Team Mercy Score
+ * @version 5-13-2019
+ */
+public class Monitor
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Monitor
+     */
+    public Monitor()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
