90f615e561b5f5e5207d202c35f36f47b670ed2e
vinodkm12 <nlvinod.km@gmail.com>
2019-05-14 14:21:03 -0700

    Added move() class

--- TheGame/Move.java
+++ TheGame/Move.java
@@ -1,33 +1,52 @@
 
 /**
- * Write a description of class Move here.
+ * Object for a Move that the player makes
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Team Mercy Score
+ * @version 5-14-2019
  */
 public class Move
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    // 1 is for reload
+    // 2 is to shoot
+    // 3 is to block
+    //4 is for nothing
+    private int myType;
 
     /**
      * Constructor for objects of class Move
      */
-    public Move()
+    public Move(int type)
     {
         // initialise instance variables
-        x = 0;
+        myType = type;
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * Accessor for the type of the move
+     * 
+     * @return int type  - type of move this is
      */
-    public int sampleMethod(int y)
+    public int getType()
     {
-        // put your code here
-        return x + y;
+        return myType;
     }
+    
+    /**
+     * Tells whether one move is equal to another
+     *
+     * @param  y  another move to compare to
+     * @return    if the move is equal return true, else return false
+     */
+    public boolean equals(Move y)
+    {
+        boolean ans = false;
+        if (getType() == y.getType())
+            ans = true;
+        return ans;
+    }
+    
+    /**
+     * Compa
+     */
 }
