205724022e01bca4b0b07e26ef6b77d55d6d7d11
vinodkm12 <nlvinod.km@gmail.com>
2019-05-14 14:16:02 -0700

    Started on Double07 class

--- TheGame/Double07.java
+++ TheGame/Double07.java
@@ -5,26 +5,77 @@
  * @author Team Mercy Score
  * @version 5-13-2019
  */
-public abstract class Double07
+public class Double07
 {
     // instance variables - replace the example below with your own
-    private int p;
+    private int numBullets;
+    private int numLives;
 
     /**
      * Constructor for objects of class Double07
+     * Set number of lives 
      */
     public Double07()
     {
-
+        numBullets = 0;
+        //U get a random number of lives from 2 to 4
+        numLives = (int) (Math.random() * 3 + 2);
     }
 
     /**
-     * An example of a method - replace this comment with your own
+     * Accessor for number of lives
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * @return    number of lives
      */
-    public void sampleMethod(int y)
+    public int getLives()
     {   
+        return numLives;
     }
+    
+    /**
+     * Modifier for number of lives
+     * 
+     * @param new number of lives
+     */
+    public void setLives(int theNumLives)
+    {
+        numLives = theNumLives;
+    }
+    
+    /**
+     * Accessor for number of bullets
+     * @return  number of bullets
+     */
+    public int getBullets()
+    {
+        return numBullets;
+    }
+    
+    /**
+     * Modifier for number of bullets
+     * @param      new number of bullets
+     */
+    public void setBullets(int newNumBullets)
+    {
+        numBullets = newNumBullets;
+    }
+    
+    /**
+     * Tells whether the player is dead
+     * @return      true if player is dead, false otherwise
+     */
+    public boolean isDead()
+    {
+        boolean ans = false;
+        if (getLives() <= 0)
+            ans = true;
+        return ans;
+    }
+    
+    public boolean isMoveAllowed(Move x)
+    {
+        //do this later
+        return true;
+    }
+    
 }
