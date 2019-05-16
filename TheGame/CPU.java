148cc4e49fa9a7a411f20bafbdfe909d2d06e25e
vinodkm12 <nlvinod.km@gmail.com>
2019-05-16 13:04:20 -0700

    Added bomments

--- TheGame/CPU.java
+++ TheGame/CPU.java
@@ -18,26 +18,36 @@ public class CPU extends Double07
         double triggerHappy =   1-opp.getBlockCounter()/(MAXBLOCKS + 1.00 + getBullets())  ;
         //Probability of blocking
         double willIBlock;
-        
         Move temp, ans;
         
+        //If the random process selects for a shoot and it is allowed, do it
         temp  = new Move(2);
         if (  myRandomVar < triggerHappy && isMoveAllowed(temp)  )
             ans = temp;
+        //Otherwise, choose between blocking and reloading
         else
         {
+            //Reload the random variable to avoid possible problems
             myRandomVar = Math.random();
-            willIBlock = 3.00/(3.00+ ( getBlockCounter() )*( Math.log(opp.getBullets()) )  );
+            //Use a formula to calculate the probability of blocking
+            //The probability of blocking is related to CPU number of block counters
+                //and the opponents number of bullets
+            willIBlock = 1.000 - (3.00/(3.00+ ( getBlockCounter() )*( Math.log(opp.getBullets()) )  ));
+            //If the random variable falls in the right range for a block
             if(myRandomVar < willIBlock)
             {
+                //If the block is allowed
                 if(isMoveAllowed(new Move(3)))
                     ans = new Move(3);
+                //If the block isn't allowed, we gotta do a reload
                 else
                     ans = new Move(1);
             }
             else
+            //The last choice is doing a reload
                 ans = new Move(1);
         }
+        //Load the found move in currentMove
         currentMove = ans;
     }
 }
