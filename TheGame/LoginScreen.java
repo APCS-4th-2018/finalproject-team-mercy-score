549d926e3957948efae0825a0d4d1b1672fb3cd9
vinodkm12 <nlvinod.km@gmail.com>
2019-05-28 14:20:54 -0700

    Finish this text thing later

--- TheGame/LoginScreen.java
+++ TheGame/LoginScreen.java
@@ -6,7 +6,8 @@ import javafx.scene.Scene;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
 
@@ -29,10 +30,12 @@ public class LoginScreen extends Application
     {
         GridPane grid = new GridPane();
         grid.setHgap(100);
+        grid.setAlignment(Pos.CENTER);
         grid.setVgap(100);
         grid.setGridLinesVisible(true);
         Text credits = new Text("TEAM MERCY SCORE");
-        grid.add(credits, 2, 2);
+        credits.setFontSize(getFontSize()+10);
+        grid.add(credits, 0,0);
         //grid.add(new Text("TEAM MERCY SCORE"), 0, 1);
         Scene myScene = new Scene(grid, 500, 500);
         myStage.setScene(myScene);
